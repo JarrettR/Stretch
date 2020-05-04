2f836a8ede1c613e973500ffbdd99242bd1480bf
JarrettR <jrainier@gmail.com>
2020-05-03 17:21:40 -0700

    Load PCB -> Save PCB pipeline works

--- sexpressions_writer.py
+++ sexpressions_writer.py
@@ -1,17 +1,75 @@
+import io
 from bs4 import BeautifulSoup
+import json
+import re
 
-
+from parser_base import ParserBase
+from sexpressions_parser import parse_sexpression
 
 
 class SexpressionWriter(object):
-    def __init__(self, svg):
+    def __init__(self):
+        self.filename_in = "example/out.svg"
+        self.filename_out = "example/out.kicad_pcb"
+        self.filename_sexpression = "example/simple.kicad_pcb"
+
+    def Load(self):
+        with open(self.filename_in, "r") as f:
+    
+            contents = f.read()
+            svg = BeautifulSoup(contents, 'html.parser')
+            return svg
+
+    def Load_Sexpression(self):
+        with io.open(self.filename_sexpression, 'r', encoding='utf-8') as f:
+            sexpression = parse_sexpression(f.read())
+        return sexpression
 
 
-        # May or may not need to actually use an XML parser instead of HTML!
-        #self.soup = BeautifulSoup(svg, 'lxml-xml')
-        self.soup = BeautifulSoup(svg, 'html.parser')
+
+    def List_Escape(self, lst):
+        newlist = []
+        RE = re.compile('[\\\/\?%\(\)\ ]|^$')
+
+        for value in lst:
+            if type(value) is not list:
+                if RE.search(value):
+                    value = '"' + value + '"'
+
+            newlist.append(value)
+
+        return newlist
 
 
-    def display(self):
+    def List_To_Sexpression(self, lst, first = True):
+        line = []
+
+        lst = self.List_Escape(lst)
+
+        for value in lst:
+            if type(value) is list:
+                line.append('(' + self.List_To_Sexpression(value, False) + ')\n')
+            else:
+                line.append(value)
+
+        out = ' '.join(line)
+
+        if first:
+            out = '(' + out + ')'
+
+        return out
+
+    def Save(self, sexpression):
+        with open(self.filename_out, 'w') as f:
+            f.write(sexpression)
+
+    def Run(self):
+        # svg = self.Load()
+        lst = self.Load_Sexpression()
+        sexpression = self.List_To_Sexpression(lst)
+        self.Save(sexpression)
         
-        print(self.soup.prettify())
+
+if __name__ == '__main__':
+    e = SexpressionWriter()
+    e.Run()
