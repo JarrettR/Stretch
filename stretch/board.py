9187f040b81f2c176d816a88037d4c88ffeba619
JarrettR <jrainier@gmail.com>
2021-01-07 08:54:02 -0800

    Listed board-level components

---
+++ stretch/board.py
@@ -0,0 +1,30 @@
+https://github.com/KiCad/kicad-source-mirror/blob/93466fa1653191104c5e13231dfdc1640b272777/pcbnew/plugins/kicad/pcb_parser.cpp#L533
+
+
+class Board(object):
+
+    def __init__(self):
+        self.general = ''
+        self.paper = ''
+        self.title_block = ''
+        self.layers = ''
+        self.setup = ''
+        self.property = ''
+        self.net = ''
+        self.net_class = ''
+        self.gr_arc = ''
+        self.gr_curve = ''
+        self.gr_line = ''
+        self.gr_poly = ''
+        self.gr_circle = ''
+        self.gr_rect = ''
+        self.gr_text = ''
+        self.gr_dimension = ''
+        self.module = ''
+        self.footprint = ''
+        self.segment = ''
+        self.arc = ''
+        self.group = ''
+        self.via = ''
+        self.zone = ''
+        self.target = ''
