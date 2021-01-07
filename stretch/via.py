0a71b5c0e820ec040ac485c37110981bcd57af9d
JarrettR <jrainier@gmail.com>
2021-01-06 23:23:10 -0800

    Cleaned up general structure for via

--- stretch/via.py
+++ stretch/via.py
@@ -1,149 +1,223 @@
+https://github.com/KiCad/kicad-source-mirror/blob/93466fa1653191104c5e13231dfdc1640b272777/pcbnew/plugins/kicad/pcb_parser.cpp#L4275
+
+# 0 via
+# 1
+#   0 at
+#   1 66.66
+#   2 99.99
+# 2
+#   0 size
+#   1 0.6
+# 3
+#   0 drill
+#   1 0.3
+# 4
+#   0 layers
+#   1 F.Cu
+#   2 B.Cu
+# 5
+#   0 net
+#   1 16
+
+
 class Via(object):
 
-
     def __init__(self):
-    
-        self.isModule = False
-        self.x = 0
-        self.y = 0
+        self.blind = False
+        self.micro = False
+        seld.at = []
         self.size = 0
         self.drill = 0
         self.layers = []
         self.net = 0
+        self.remove_unused_layers = False
+        self.keep_end_layers = False
         self.tstamp = ''
+        self.status = ''
+        self.locked = False
+        self.free = False
 
 
-    def Parse_Vias(self, tag):
-        # (via (at 205.486 133.731) (size 0.6) (drill 0.3) (layers F.Cu B.Cu) (net 0) (tstamp 5EA04144) (status 30))
-        vias = []
-        for via_in in tag.find_all('g'):
-            if not via_in.has_attr('type'):
-                continue
-            if not via_in['type'] == "via":
-                continue
-            x = via_in['x']
-            y = via_in['y']
-            at = ['at', str(float(x) / pxToMM), str(float(y) / pxToMM)]
+    def From_SVG(self, tag):
+        if not tag.has_attr('type'):
+            break
+        if not tag['type'] == "via":
+            break
+            
+        x = tag['x']
+        y = tag['y']
+        self.at = [(float(x) / pxToMM), (float(y) / pxToMM)]
 
-            via = [ 'via', at, ['size', via_in['size']], ['drill', via_in['drill']]]
+        self.size = float(tag['size']) / pxToMM
+        self.drill = float(tag['drill']) / pxToMM
 
-            layers = via_in['layers'].split(',')
-            layers = ['layers'] + layers
+        self.layers = tag['layers'].split(',')
 
-            via.append(layers)
+        self.net = tag['net']])
 
-            via.append(['net', via_in['net']])
+        if tag.has_attr('tstamp'):
+            self.tstamp = tag['tstamp']
+        if tag.has_attr('status'):
+            self.status = tag['status']
+        if tag.has_attr('blind'):
+            self.blind = True
+        if tag.has_attr('micro'):
+            self.micro = True
+        if tag.has_attr('remove_unused_layers'):
+            self.remove_unused_layers = True
+        if tag.has_attr('keep_end_layers'):
+            self.keep_end_layers = True
+        if tag.has_attr('locked'):
+            self.locked = True
+        if tag.has_attr('free'):
+            self.free = True
 
-            if via_in.has_attr('tstamp'):
-                via.append(['tstamp', via_in['tstamp']])
-            if via_in.has_attr('status'):
-                via.append(['status', via_in['status']])
 
-            vias.append(via)
-        return vias
+    def To_PCB(self):
+        at = ['at'] + self.at
+
+        via = [ 'via', at, ['size', self.size], ['drill', self.drill]]
+
+        via.append(['layers'] + self.layers)
+
+        via.append(['net', self.net])
+
+        if self.tstamp != '':
+            via.append(['tstamp', self.tstamp])
+        if self.status != '':
+            via.append(['status', self.status])
+        if self.blind != False:
+            via.append(['blind'])
+        if self.micro != False:
+            via.append(['micro'])
+        if self.remove_unused_layers != False:
+            via.append(['remove_unused_layers'])
+        if self.keep_end_layers != False:
+            via.append(['keep_end_layers'])
+        if self.locked != False:
+            via.append(['locked'])
+        if self.free != False:
+            via.append(['free'])
+
+        return via
        
 
-       def Convert_Via_To_SVG(self, input, id):
-        # 0 via
-        # 1
-        #   0 at
-        #   1 66.66
-        #   2 99.99
-        # 2
-        #   0 size
-        #   1 0.6
-        # 3
-        #   0 drill
-        #   1 0.3
-        # 4
-        #   0 layers
-        #   1 F.Cu
-        #   2 B.Cu
-        # 5
-        #   0 net
-        #   1 16
-
+    def From_PCB(self, pcblist):
         at = []
         layers = []
         blind = ''
         status = ''
         tstamp = ''
 
-        if input[0] != 'via':
+        if pcblist[0] != 'via':
             assert False,"Via: Not a via"
             return None
 
-
-        for item in input:
+        for item in pcblist:
             if item[0] == 'at':
-                at.append(item[1])
-                at.append(item[2])
+                self.at = [item[1], item[2]]
 
             if item[0] == 'size':
-                size = item[1]
+                self.size = item[1]
 
             if item[0] == 'drill':
-                drill = item[1]
+                self.drill = item[1]
 
             if item[0] == 'layers':
-                layers.append(item[1])
-                layers.append(item[2])
+                self.layers = [item[1], item[2]]
 
             if item[0] == 'net':
-                net = item[1]
+                self.net = item[1]
 
             if item == 'blind':
-                blind = 'blind=true '
+                self.blind = True
 
             if item[0] == 'tstamp':
-                tstamp = 'tstamp="' + item[1] + '" '
+                self.tstamp = item[1] 
             
             if item[0] == 'status':
-                status = 'status="' + item[1] + '" '
-         
+                self.status = item[1]
+                
+                
+    def To_SVG(self):
+        tstamp = ''
+        status = ''
+        blind = ''
+        micro = ''
+        remove_unused_layers = ''
+        keep_end_layers = ''
+        locked = ''
+        free = ''
+        
+        if self.tstamp != '':
+            tstamp = 'tstamp="' + self.tstamp + '" '
+        if self.status != '':
+            status = 'status="' + self.status + '" '
+        if self.blind != False:
+            blind = 'blind="true" '
+        if self.micro != False:
+            micro = 'micro="true" '
+        if self.remove_unused_layers != False:
+            remove_unused_layers = 'remove_unused_layers="true" '
+        if self.keep_end_layers != False:
+            keep_end_layers = 'keep_end_layers="true" '
+        if self.locked != False:
+            locked = 'locked="true" '
+        if self.free != False:
+            free = 'free="true" '
+
         parameters = '<g '
-        parameters += 'x="' + str(float(at[0]) * pxToMM) + '" '
-        parameters += 'y="' + str(float(at[1]) * pxToMM) + '" '
-        parameters += 'id="via' + str(id) + '" '
+        parameters += 'x="' + str(float(self.at[0]) * pxToMM) + '" '
+        parameters += 'y="' + str(float(self.at[1]) * pxToMM) + '" '
+        #parameters += 'id="via' + str(id) + '" '
         parameters += 'type="via" '
-        parameters += 'layers="' + layers[0] + ',' + layers[1] + '" '
-        parameters += 'size="' + size + '" '
-        parameters += 'drill="' + drill + '" '
-        parameters += 'net="' + net + '" '
+        parameters += 'layers="' + self.layers[0] + ',' + self.layers[1] + '" '
+        parameters += 'size="' + self.size + '" '
+        parameters += 'drill="' + self.drill + '" '
+        parameters += 'net="' + self.net + '" '
         parameters += blind
         parameters += tstamp
         parameters += status
+        parameters += micro
+        parameters += remove_unused_layers
+        parameters += keep_end_layers
+        parameters += locked
+        parameters += free
         parameters += '>'
 
         hole = '<circle style="stroke:none;stroke-linecap:round;stroke-linejoin:miter;fill-opacity:1'
         hole += ';fill:#' + self.Assign_Layer_Colour('Via.Inner')
         hole += '" '
-        hole += 'cx="' + str(float(at[0]) * pxToMM) + '" '
-        hole += 'cy="' + str(float(at[1]) * pxToMM) + '" '
-        hole += 'id="viai' + str(id) + '" '
+        hole += 'cx="' + str(float(self.at[0]) * pxToMM) + '" '
+        hole += 'cy="' + str(float(self.at[1]) * pxToMM) + '" '
+        #hole += 'id="viai' + str(id) + '" '
         #hole += 'drill="true" '
-        hole += 'r="' + str(float(drill)  * (pxToMM / 2)) + '" '
+        hole += 'r="' + str(float(self.drill)  * (pxToMM / 2)) + '" '
         hole += '/>'
 
         parameters += '<circle style="stroke:none;stroke-linecap:round;stroke-linejoin:miter;fill-opacity:1'
         parameters += ';fill:#' + self.Assign_Layer_Colour('Via.Outer')
         parameters += '" '
-        parameters += 'cx="' + str(float(at[0]) * pxToMM) + '" '
-        parameters += 'cy="' + str(float(at[1]) * pxToMM) + '" '
-        parameters += 'id="viao' + str(id) + '" '
-        parameters += 'r="' + str(float(size)  * (pxToMM / 2)) + '" '
-        parameters += 'layers="' + layers[0] + ',' + layers[1] + '" '
-        parameters += 'size="' + size + '" '
-        parameters += 'drill="' + drill + '" '
-        parameters += 'net="' + net + '" '
+        parameters += 'cx="' + str(float(self.at[0]) * pxToMM) + '" '
+        parameters += 'cy="' + str(float(self.at[1]) * pxToMM) + '" '
+        #parameters += 'id="viao' + str(id) + '" '
+        parameters += 'r="' + str(float(self.size)  * (pxToMM / 2)) + '" '
+        parameters += 'layers="' + self.layers[0] + ',' + self.layers[1] + '" '
+        parameters += 'size="' + self.size + '" '
+        parameters += 'drill="' + self.drill + '" '
+        parameters += 'net="' + self.net + '" '
         parameters += blind
         parameters += tstamp
         parameters += status
+        parameters += micro
+        parameters += remove_unused_layers
+        parameters += keep_end_layers
+        parameters += locked
+        parameters += free
         parameters += '/>' + hole 
 
         parameters += '</g>'
 
         #print(parameters)
         return parameters
- 
         
