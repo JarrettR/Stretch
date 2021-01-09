60e8c832c14690993dd1d5fbc21103bb89401dbb
JarrettR <jrainier@gmail.com>
2021-01-09 00:18:03 -0800

    Python imports

---
+++ stretch/__init__.py
@@ -0,0 +1,29 @@
+__all__ = [
+    'Arc',
+    'Board',
+    'Curve',
+    'Layer',
+    'Line',
+    'Metadata',
+    'Module',
+    'Pad',
+    'Poly',
+    'Segment',
+    'Text',
+    'Via',
+    'Zone'
+]
+
+from .arc import Arc
+from .board import Board
+from .curve import Curve
+from .layer import Layer
+from .line import Line
+from .metadata import Metadata
+from .module import Module
+from .pad import Pad
+from .poly import Poly
+from .segment import Segment
+from .text import Text
+from .via import Via
+from .zone import Zone
